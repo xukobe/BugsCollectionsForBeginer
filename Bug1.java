d1512e36b05cd6520f7eb55a81cdf1a5f451138f
lili <lilyhu8131@gmail.com>
2018-07-24 10:54:57 -0400

    Fixed pass by reference issue

--- Bug1.java
+++ Bug1.java
@@ -7,10 +7,21 @@ public class Bug1 {
 	 * otherwise return true.
 	 * The value generated is passed back by parameter.
 	 */
-	public static boolean generateRandomEvenValue(int value) {
+	private int val;
+	public Bug1(int a) {
+		this.val=a;
+	}
+	public void setVal(int b) {
+		val=b;
+	}
+	public int getVal() {
+		return val;
+	}
+	public static boolean generateRandomEvenValue(Bug1 value) {
 		Random r = new Random();
-		value = r.nextInt(100);
-		if (value % 2 == 0) {
+		value.setVal(r.nextInt(100));
+		int a=value.getVal();
+		if (a % 2 == 0) {
 			return true;
 		}
 		return false;
@@ -18,15 +29,22 @@ public class Bug1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int random_value = -1;
+		Bug1 random_value = new Bug1(-1);
 		int counter = 0;
 		while(generateRandomEvenValue(random_value)) {
 			counter++;
-			System.out.println("Generated random value is even: " + random_value);
+			System.out.println("Generated random value is even: " + random_value.getVal());
 		}
 		
 		System.out.println("Even numbers are generated continously for " + counter + " times.");
 		
+		Bug1[] b=new Bug1[2];
+		for(int i = 0; i < 2; i++) {
+			b[i] = new Bug1(i);
+		}
+		System.out.println(b);
+		System.out.println(b[0]);
+		System.out.println(b[1]);
 	}
 
 }
